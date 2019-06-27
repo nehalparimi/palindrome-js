ab9b2b9e9c73a218653b6d2ca47cb5cd5598682c
Nehal Parimi <parimi.nehal@gmail.com>
2019-06-26 22:33:02 -0700

    Initialize repository

---
+++ index.js
@@ -0,0 +1,20 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function () {
+    return Array.from(this).reverse().join("");
+}
+
+function Phrase(content) {
+    this.content = content;
+
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase();
+    }
+
+    this.palindrome = function palindrome() {
+        return this.processedContent() === this.processedContent().reverse();
+    }
+
+    this.louder = function louder() {
+        return this.content.toUpperCase();
+    }
+}
